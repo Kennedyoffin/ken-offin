63c1984f308106cedb73a67a6c5e33754e18912e
Kennedyoffin <125225631+Kennedyoffin@users.noreply.github.com>
2023-02-13 11:50:42 +0000

    Add files via upload

---
+++ kennedy Offin.py
@@ -0,0 +1,11 @@
+
+
+#Multiplication Times Table with Python
+Num= int(input("Enter number for the multiplication table: "))
+print("The Multiplication Table of:",Num)
+for count in range(1,13):
+    print(Num,"x",count,"=",Num*count)
+
+
+
+input("Press ENTER to Exit")
